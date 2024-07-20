b5e36b1554568d02c3e5868b874a75751da0f239
RumiCh08 <mailmerumi08@gmail.com>
2024-07-20 18:40:19 +0530

    Subtask 3

--- pyls.py
+++ pyls.py
@@ -8,20 +8,25 @@ with open('structure.json') as f:
     data = json.load(f)
 
 # Function to print the directory contents
-def print_ls(contents, show_all=False):
+def print_ls(contents, show_all=False, long_format=False):
     for item in contents:
         if not show_all and item['name'].startswith('.'):
             continue
-        print(item['name'], end=' ')
-    print()
+        if long_format:
+            print(f"{item['permissions']} {item['size']} {item['name']}")
+        else:
+            print(item['name'], end=' ')
+    if not long_format:
+        print()
 
 #Argument parser
 parser = argparse.ArgumentParser(description='A simple implementation of ls command in Python')
 parser.add_argument('-A', action='store_true', help='do not ignore entries starting with .')
+parser.add_argument('-l', action='store_true', help='use a long listing format')
 args = parser.parse_args()
 
 # Get the top-level contents
 top_level_contents = data['contents']
 
 # Print the top-level contents
-print_ls(top_level_contents, show_all=args.A)
+print_ls(top_level_contents, show_all=args.A, long_format=args.l)
