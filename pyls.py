9a33a7acaea9c6979eb858eba4501c16c25e8e3f
RumiCh08 <mailmerumi08@gmail.com>
2024-07-20 18:13:06 +0530

    Subtask 2

--- pyls.py
+++ pyls.py
@@ -1,4 +1,7 @@
 import json
+import sys
+import argparse
+
 
 # Load the JSON data
 with open('structure.json') as f:
@@ -12,8 +15,13 @@ def print_ls(contents, show_all=False):
         print(item['name'], end=' ')
     print()
 
+#Argument parser
+parser = argparse.ArgumentParser(description='A simple implementation of ls command in Python')
+parser.add_argument('-A', action='store_true', help='do not ignore entries starting with .')
+args = parser.parse_args()
+
 # Get the top-level contents
 top_level_contents = data['contents']
 
 # Print the top-level contents
-print_ls(top_level_contents)
+print_ls(top_level_contents, show_all=args.A)
